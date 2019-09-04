68d0e7c867526582ec43d97a3b8d39cdc643963a
Dongyu Zhao <donz@microsoft.com>
2019-09-04 15:15:05 +0800

    Change CheckBox icon

--- source/src/Basics/CheckBox.tsx
+++ source/src/Basics/CheckBox.tsx
@@ -12,7 +12,7 @@ const Checked = (props: CheckMarkProps) => {
             style={props.style}
             viewBox='0 0 24 24'
         >
-            <Path d='M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z' fill={props.color} />
+            <Path d='M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-8.29 13.29c-.39.39-1.02.39-1.41 0L5.71 12.7c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0L10 14.17l6.88-6.88c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41l-7.58 7.59z' fill={props.color} />
         </Svg>
     );
 };
@@ -25,7 +25,7 @@ const Unchecked = (props: CheckMarkProps) => {
             style={props.style}
             viewBox='0 0 24 24'
         >
-            <Path d='M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z' fill={props.color} />
+            <Path d='M18 19H6c-.55 0-1-.45-1-1V6c0-.55.45-1 1-1h12c.55 0 1 .45 1 1v12c0 .55-.45 1-1 1zm1-16H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z' fill={props.color} />
         </Svg>
     );
 };
@@ -38,7 +38,7 @@ const Intermediate = (props: CheckMarkProps) => {
             style={props.style}
             viewBox='0 0 24 24'
         >
-            <Path d='M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 10H7v-2h10v2z' fill={props.color} />
+            <Path d='M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-3 10H8c-.55 0-1-.45-1-1s.45-1 1-1h8c.55 0 1 .45 1 1s-.45 1-1 1z' fill={props.color} />
         </Svg>
     );
 };
